36e883b734547fec4af8566e32a4f82493a848ba
TomasBruh <tomas.maksele@gmail.com>
2022-01-25 19:56:43 +0200

    1paskDone

---
+++ 1pask/main.py
@@ -0,0 +1,37 @@
+# TODO: Touch grass
+# SHIFT + ENTER
+# CTRL + SLASH
+# print(type())
+# snake_case
+# Didžiosios raidės - konstantos
+# cmd - py
+
+print("Hello world")
+print(type(1010))
+
+URL = "https://www.pyhton.org"
+
+my_name = "Tomas"
+# print(my_name.upper())
+# print(my_name.isupper())
+# print(my_name.isalnum())
+# print((my_name.isalpha()))
+# print(my_name.lower())
+# print(my_name.islower())
+
+
+my_age = 15
+my_latitude = 54.727082
+my_longitude = 25.234908
+
+print("My name is: " + my_name)
+print(f"My age is: {my_age}")
+print("My latitude is: " + str(my_latitude))
+print(f"My longitude is: {my_longitude}")
+print("\n")
+print(f"""My name is: {my_name}
+My age is: {my_age}
+My latitude: {my_latitude}
+My longitude: {my_longitude}""")
+
+print(f"My name is {my_name}\nMy age is {my_age}\nMy latitude {my_latitude}\nMy longitude {my_longitude}")
