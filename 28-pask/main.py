941ab1abd7176deb40cc0398054014a19c0c9bc4
TomasBruh <tomas.maksele@gmail.com>
2022-05-24 21:45:28 +0300

    29

--- 28-pask/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import engine
-from routers import user_routers
-import models
-
-app = FastAPI()
-
-app.include_router(user_routers.router)
-models.Base.metadata.create_all(engine)
